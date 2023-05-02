bc355b627562a9ee9e628c7912be7c889667c480
hayeonlee12 <54343481+hayeonlee12@users.noreply.github.com>
2023-05-02 16:00:55 +0900

    파일 수정

--- workspace/springshop/src/main/java/com/codingbox/springshop/domain/Order.java
+++ workspace/springshop/src/main/java/com/codingbox/springshop/domain/Order.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -13,7 +14,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 //import javax.persistence.OneToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="orders")
+@Getter @Setter
 public class Order {
 	@Id @GeneratedValue
 	@Column(name="ORDER_ID")
